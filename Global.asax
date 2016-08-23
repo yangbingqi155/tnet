9a766396c00db95649c8bfc33db597b8edc6f148
aixiaoxiang@richers.com.cn <aixiaoxiang@richers.com.cn>
2016-08-23 14:07:17 +0800

    签入

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TNet.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TNet.Startup))]
+namespace TNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
