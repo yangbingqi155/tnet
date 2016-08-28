dfc08fdde0ed07d75005e67ff828c54d7ef76355
aixiaoxiang@richers.com.cn <aixiaoxiang@richers.com.cn>
2016-08-28 10:54:49 +0800

    首页商品

--- Js/Home/Home.js
+++ Js/Home/Home.js
@@ -1,6 +1,5 @@
-﻿var g_connkey, g_conname;
+﻿//轮播控件
 function initPage() {
-    $(".header").hide();
     var tabsSwiper = new Swiper('.swiper-container', {
         autoplay: 3000,
         visibilityFullFit: true,
@@ -10,6 +9,58 @@ function initPage() {
         preloadImages: false,
         lazyLoading: true
     });
+    getMercList();
 }
 
+function getMercList() {
+    Pub.get({
+        url: "Service/Merc/List",
+        noLoading: true,
+        success: function (data) {
+            var html = "";
+            if (Pub.wsCheck(data)) {
+                if (data.Data) {
+                    for (var i = 0; i < data.Data.length; i++) {
+                        var lo = data.Data[i];
+                        var ro = null;
+                        if (i++ < data.Data.length) {
+                            ro = data.Data[i];
+                        }
+                        
+                        html += '<div class="pitem">';
+                        html += crateItem(lo, 'l', ro);
+                        html += crateItem(ro, 'r', null);
+                        html += ' </div>';
+
+                    }
+                }
+
+                if (html) {
+                    $('#merc').html(html);
+                }
+            }
+        },
+        error: function (data) {
+
+        }
+    });
+}
+
+function crateItem(o, tag, o2) {
+    var html = '';
+    if (o) {
+        html += '<div class="pitem_' + tag + '">';
+        html += '<a href="#">';
+        html += '<img src="'+Pub.rootUrl()+'Images/Merc/0.jpg" />';
+        html += o.merc1 + '</a>';
+        if (o2) {
+            html += '<div class="HLive"></div>';
+        }
+        html += '</div>';
+    }
+    return html;
+}
 $(document).ready(initPage);
+
+
+
