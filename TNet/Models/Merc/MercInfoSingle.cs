ad755c33845c12402d60e453e3171d736748509b
aixiaoxiang@richers.com.cn <aixiaoxiang@richers.com.cn>
2016-08-31 13:48:03 +0800

    整理目录

---
+++ TNet/Models/Merc/MercInfoSingle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace TNet.Models.Service.Merc
+{
+    
+    public class MercDataSingle
+    {
+        public EF.Merc Merc { get; set; }
+
+        public List<EF.Spec> Spec { get; set; }
+
+        public List<EF.Discount> Discount { get; set; }
+
+    }
+}
---
+++ TNet/Models/Service/R.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNet.Models.Service.Com
+{
+    /// <summary>
+    /// 各种资源定义
+    /// </summary>
+    public sealed class R
+    {
+        /// <summary>
+        /// 错误代码
+        /// </summary>
+        public static readonly int Error = -1;
+
+        /// <summary>
+        /// 失败代码
+        /// </summary>
+        public static readonly int Fail = 0;
+
+        /// <summary>
+        /// 成功代码
+        /// </summary>
+        public static readonly int Ok = 1;
+
+
+    }
+}
