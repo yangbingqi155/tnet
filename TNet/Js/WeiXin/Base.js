49e5fe89f448173e6fdfca486ed5462a0808c6de
aixiaoxiang2009@hotmail.com <aixiaoxiang2009@hotmail.com>
2016-09-02 19:05:26 +0800

    首页样式

--- TNet/Js/WeiXin/Base.js
+++ TNet/Js/WeiXin/Base.js
@@ -9,11 +9,21 @@ function initBase() {
         preloadImages: false,
         lazyLoading: true
     });
-
     var u = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc530ec3ce6a52233&redirect_uri=http://app.i5shang.com/tnet/user&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect';
     $(".Top_User").attr("href", u);
 }
-
+function autoShowTopMenu() {
+    $('#Top_Main_Menu_Host').toggle();
+    var cObj = $('#C');
+    cObj.toggle();
+    if (cObj.is(":hidden")){
+        $(document.body).removeClass("body_bg").addClass("body_bg");
+    } else {
+        $(document.body).removeClass("body_bg");
+    }
+    
+     
+}
 $(document).ready(initBase);
 
 
