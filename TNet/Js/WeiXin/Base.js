2e1b5bad01908460840256d583e825ebf3aad2df
aixiaoxiang2009@hotmail.com <aixiaoxiang2009@hotmail.com>
2016-09-02 19:19:54 +0800

    优化首页菜单体验

--- TNet/Js/WeiXin/Base.js
+++ TNet/Js/WeiXin/Base.js
@@ -12,17 +12,23 @@ function initBase() {
     var u = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc530ec3ce6a52233&redirect_uri=http://app.i5shang.com/tnet/user&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect';
     $(".Top_User").attr("href", u);
 }
+var g_base_x_v = 0;
 function autoShowTopMenu() {
-    $('#Top_Main_Menu_Host').toggle();
+    if (g_base_x_v == 0) {
+        g_base_x_v = $(document.body).scrollTop();
+    }
     var cObj = $('#C');
     cObj.toggle();
-    if (cObj.is(":hidden")){
+    $('#Top_Main_Menu_Host').toggle();
+    if (cObj.is(":hidden")) {
         $(document.body).removeClass("body_bg").addClass("body_bg");
     } else {
         $(document.body).removeClass("body_bg");
+        window.setTimeout(function () {
+            $(document).scrollTop(g_base_x_v);
+            g_base_x_v = 0;
+        }, 80);
     }
-    
-     
 }
 $(document).ready(initBase);
 
