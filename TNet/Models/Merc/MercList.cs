fc657e8445fa819480ee7d9353a6360c2049effb
aixiaoxiang2009@hotmail.com <aixiaoxiang2009@hotmail.com>
2016-09-20 11:28:42 +0800

    调整商品排序，先按类型，再按商品

---
+++ TNet/Models/Merc/MercList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TNet.Models.Merc
+{
+    public class MercList
+    {
+        public List<EF.Merc> Mercs { get; set; }
+
+        public List<EF.MercType> Types { get; set; }
+    }
+}
---
+++ TNet/Models/Order/MyOrderDetail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TNet.Models.Order
+{
+    //[NotMapped]
+    public class MyOrderDetail
+    {
+        public EF.MyOrder Order { get; set; }
+
+
+
+        public List<EF.MyOrderPress> Presses { get; set; }
+
+
+        public OrderStatusItem Status
+        {
+            get
+            {
+                return OrderStatus.get(Order != null && Order.status != null ? Order.status.Value : 0);
+            }
+            set
+            {
+
+            }
+        }
+
+
+
+
+    }
+}
