57624917b0b4ddc3538d127190b70584affdd033
aixiaoxiang2009@hotmail.com <aixiaoxiang2009@hotmail.com>
2016-09-23 16:09:48 +0800

    后台样式

---
+++ TNet/Js/Manage/Base.js
@@ -0,0 +1,17 @@
+﻿
+function initParam() {
+    var href = window.location.href + "";
+    
+    if ((/(Manage\/MercList)/gi).test(href) || (/(Manage\/MercEdit)/gi).test(href)) {
+        $("#MercList").addClass("select");
+    } else if ((/(Manage\/MercTypeList)/gi).test(href) || (/(Manage\/MercTypeEdit)/gi).test(href)) {
+        $("#MercTypeList").addClass("select");
+    } else if ((/(Manage\/OrderList)/gi).test(href)) {
+        $("#OrderList").addClass("select");
+    } else if ((/(Manage\/BusinessList)/gi).test(href) || (/(Manage\/BusinessEdit)/gi).test(href)) {
+        $("#BusinessList").addClass("select");
+    }
+
+}
+
+$(document.body).ready(initParam);
